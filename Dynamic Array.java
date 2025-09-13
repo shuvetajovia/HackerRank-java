38abcc8d2dfd22a02c21b9f8c3f63430e4f1fcd9
A Shuveta Jovi <shuvetajovi2006a@gmail.com>
2025-09-13 15:02:36 +0530

    Create Dynamic Array.java

---
+++ Dynamic Array.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+   public static List<Integer> dynamicArray(int n, List<List<Integer>> queries) {
+        List<List<Integer>> seqList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            seqList.add(new ArrayList<>());
+        }
+
+        int lastAnswer = 0;
+        List<Integer> results = new ArrayList<>();
+
+        for (List<Integer> query : queries) {
+            int type = query.get(0);
+            int x = query.get(1);
+            int y = query.get(2);
+
+            int idx = (x ^ lastAnswer) % n;
+
+            if (type == 1) {
+                seqList.get(idx).add(y);
+            } else if (type == 2) {
+                int size = seqList.get(idx).size();
+                lastAnswer = seqList.get(idx).get(y % size);
+                results.add(lastAnswer);
+            }
+        }
+
+        return results;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int q = Integer.parseInt(firstMultipleInput[1]);
+
+        List<List<Integer>> queries = new ArrayList<>();
+
+        IntStream.range(0, q).forEach(i -> {
+            try {
+                queries.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        List<Integer> result = Result.dynamicArray(n, queries);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
